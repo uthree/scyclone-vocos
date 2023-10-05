1017ede6ce00d23d611b80f131ebc288acc7c8dd
uthree <uthree271828@gmail.com>
2023-10-06 07:49:25 +0900

    fix

--- spectrogram.py
+++ spectrogram.py
@@ -1,8 +1,16 @@
 import torch
 import matplotlib.pyplot as plt
 
+
+global hann_window
+hann_window = {}
+
 def spectrogram(x):
-    x = torch.stft(x, n_fft=1024, hop_length=256, center=True, return_complex=True).abs()
+    if str(x.device) not in hann_window:
+        hann_window[str(x.device)] = torch.hann_window(1024).to(x.device)
+    w = hann_window[str(x.device)]
+    x = torch.stft(x, n_fft=1024, hop_length=256, center=True,
+                   return_complex=True, normalized=False, window=w).abs()
     return x[:, :, 1:]
 
 def plot_spectrogram(x, save_path="./spectrogram.png", log=True):
